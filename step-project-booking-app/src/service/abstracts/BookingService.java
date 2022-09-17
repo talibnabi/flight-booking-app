4f7042f42a78dd50465d5aab31e9e885cbc9f3c6
talibnabi <talibnabiyev@gmail.com>
2022-09-17 01:26:15 -0700

    commit

--- step-project-booking-app/src/service/abstracts/BookingService.java
+++ step-project-booking-app/src/service/abstracts/BookingService.java
@@ -1,4 +1,14 @@
 package service.abstracts;
 
+import entity.Booking;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BookingService {
+    Optional<List<Booking>> getAllBookingByUser();
+
+    Boolean booking();
+
+    Boolean cancelBooking();
 }
