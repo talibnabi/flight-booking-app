7c32159fea11a9aa07c2bab7098ce290402ee0b8
talibnabi <talibnabiyev@gmail.com>
2022-09-20 09:07:47 -0700

    commit

--- step-project-booking/src/app/ApplicationMainMenu.java
+++ step-project-booking/src/app/ApplicationMainMenu.java
@@ -1,12 +1,10 @@
 package app;
-import console.concretes.FlightBookingMenuManager;
 import exception.*;
 
 import static util.MenuUtil.userMenuManager;
 
 public class ApplicationMainMenu {
     public static void showMenuInfo(){
-        FlightBookingMenuManager flightBookingMenuManager = new FlightBookingMenuManager();
         try {
             userMenuManager.menu();
         } catch (UserMenuValueNotFoundException | UserPasswordDoesntMatcherException | UserNotFoundException |
