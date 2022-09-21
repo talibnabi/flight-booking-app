e2fd26abd598cd41e4a7168aaba5f2adf299cfdb
talibnabi <talibnabiyev@gmail.com>
2022-09-21 00:34:24 -0700

    commit

--- flight-booking-step-project/src/test/java/dao/UserDAOTest.java
+++ flight-booking-step-project/src/test/java/dao/UserDAOTest.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class UserDAOTest {
+import dao.concretes.UserDAOManager;
+import entity.User;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserDAOTest {
+    @Test
+    void getAllUser() {
+        UserDAOManager userDAOManager=new UserDAOManager();
+        User user = new User("rasim", "rasim123");
+        userDAOManager.create(user);
+        ArrayList<User> users = new ArrayList<>();
+        users.add(user);
+        assertEquals(users.get(0).toString(), user.toString());
+    }
+    
 }
