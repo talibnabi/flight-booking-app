01dd88bebc8ecb6c375360780deb7ecdc8aac06c
talibnabi <talibnabiyev@gmail.com>
2022-09-21 00:37:49 -0700

    commit

--- flight-booking-step-project/src/test/java/dao/UserDAOTest.java
+++ flight-booking-step-project/src/test/java/dao/UserDAOTest.java
@@ -5,18 +5,33 @@ import entity.User;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserDAOTest {
+    private static final UserDAOManager userDAOManager = new UserDAOManager();
+
     @Test
     void getAllUser() {
-        UserDAOManager userDAOManager=new UserDAOManager();
         User user = new User("rasim", "rasim123");
         userDAOManager.create(user);
         ArrayList<User> users = new ArrayList<>();
         users.add(user);
         assertEquals(users.get(0).toString(), user.toString());
     }
-    
+
+    @Test
+    void getUserById() {
+        User user = new User("rasim", "rasim123");
+        userDAOManager.create(user);
+        assertEquals(Optional.of(user), userDAOManager.getById(user.getId()));
+    }
+
+    @Test
+    void add() {
+        User user = new User("rasim", "rasim123");
+        assertTrue(userDAOManager.create(user));
+    }
 }
